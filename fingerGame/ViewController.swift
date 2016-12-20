e377b1beddd3681523ba820491de80459f773847
Kondor <Valentin@Valentins-MacBook-Air.local>
2016-12-20 23:19:22 +0100

    Initial Commit

---
+++ fingerGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fingerGame
+//
+//  Created by Valentin Kalinov on 20/12/2016.
+//  Copyright © 2016 Valentin Kalinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
